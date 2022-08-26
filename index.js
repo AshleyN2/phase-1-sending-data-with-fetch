e4eb00b4edb7f387c71474007e44afee8c5d2023
Ashley <ashleynjeri7@gmail.com>
2022-08-26 15:27:45 +0300

    Completed assignmet

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Add your code here
+function submitData (name, email ) {
+
+  return fetch( "http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name : "Steve",
+        email : "steve@steve.com",
+      } )
+    } )
+    .then (function (response) {
+      return response.json()
+    } )
+    .then (function (object) {
+      document.body.innerHTML = object.id
+    } )
+    .catch (function (error) {
+      alert ("Unauthorized Access")
+      document.body.innerHTML = error.message
+    } )
+} 
+
+
+
+
+
+
+
+/*
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    });
+  
+  */
